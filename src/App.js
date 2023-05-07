90993851c87f0e3c03aaf9295938675fde643b36
Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>
2023-05-07 15:42:56 -0500

    correccion boton se agrega funcion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
+    let num = 1;
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{num}</span>
+        <button
+          id="inc"
+          onClick={function () {
+            return num++;
+          }}
+        >
+          Incrementa
+        </button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-it('renders without crashing', () => {
+it("renders without crashing", () => {
   shallow(<App />);
 });
 
-it('renders the value', () => {
+it("renders the value", () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.find('.value').length).toBe(1);
+  expect(wrapper.find(".value").length).toBe(1);
 });
 
-it('renders the button', () => {
+it("renders the button", () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.find('button#inc').length).toBe(1);
+  expect(wrapper.find("button#inc").length).toBe(1);
 });
 
-it('increments the value', () => {
+it("increments the value", () => {
   const wrapper = shallow(<App />);
-  wrapper.find('button').simulate('click');
-  expect(wrapper.find('.value').text()).toBe("1");
+  wrapper.find("button").simulate("click");
+  expect(wrapper.find(".value").text()).toBe("1");
 });
